4cc42331f0a7bf385ada287ba6cfadd0ceaca7b6
Eben Coetzer <eben@aves.co.za>
2023-07-21 13:54:47 +0200

    Dicepting into different components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './assets/investment-calculator-logo.png';
+import logo from "./assets/investment-calculator-logo.png";
 
 function App() {
   const calculateHandler = (userInput) => {
     // Should be triggered when form is submitted
-    // You might not directly want to bind it to the submit event on the form though...
+    // Might not directly want to bind it to the submit event on the form...
 
     const yearlyData = []; // per-year results
 
-    let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
-    const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
-    const expectedReturn = +userInput['expected-return'] / 100;
-    const duration = +userInput['duration'];
+    let currentSavings = +userInput["current-savings"]; // change the shape of this input object
+    const yearlyContribution = +userInput["yearly-contribution"]; // change the shape...
+    const expectedReturn = +userInput["expected-return"] / 100;
+    const duration = +userInput["duration"];
 
     // The below code calculates yearly results (total savings, interest etc)
     for (let i = 0; i < duration; i++) {
       const yearlyInterest = currentSavings * expectedReturn;
       currentSavings += yearlyInterest + yearlyContribution;
       yearlyData.push({
-        // feel free to change the shape of the data pushed to the array!
+        // change the shape of the data pushed to the array
         year: i + 1,
         yearlyInterest: yearlyInterest,
         savingsEndOfYear: currentSavings,
